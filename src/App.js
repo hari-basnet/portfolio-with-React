dae74ac93e624df41f74a07988cf294d16740336
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-06-01 18:50:02 +0300

    added HeroImage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import HeroImage from './components/HeroImage';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-
+      <div className="App">
         <h1>This is my portfolio</h1>
+        <HeroImage />
 
       </div>
 
---
+++ src/components/HeroImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HeroImage = () => {
+    return (
+        <div className="hero-image">
+            Hero Image is here
+        </div>
+    )
+}
+
+export default HeroImage;
